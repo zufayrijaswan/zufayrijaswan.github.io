5121679e8e77fbac01e38f28b2ba68ab6188d8ac
zufayrijaswan <zufayrijaswan.zj@gmail.com>
2024-06-13 20:08:54 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const question = document.querySelector(".question")
+const gif = document.querySelector(".gif")
+const yesBtn = document.querySelector(".yes-btn")
+const noBtn = document.querySelector(".no-btn")
+
+yesBtn.addEventListener("click", ()=> {
+    question.innerHTML = "YAAHUUU!!!";
+    gif.src="https://gifdb.com/images/high/anime-hug-rosario-vampire-tzze6znw5lnglmic.gif"
+})
+
+noBtn.addEventListener("mouseover", ()=> {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX)
+    const randomY = Math.floor(Math.random() * maxY)
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+})
